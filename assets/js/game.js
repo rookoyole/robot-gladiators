3d285f6c543e0ac4bb253cb9d24831afa9f71a52
Kyle Ryan <rookoyole@gmail.com>
2021-02-09 23:20:36 -0700

    fight all enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,9 +17,12 @@ var enemyAttack = 12;
 // fight function
 var fight = function(enemyName) {
   // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  //window.alert("Welcome to Robot Gladiators!");
 
-  // ask player if they'd like to fight or run
+  // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
+    // place fight function code block here . . .
+     // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
   // if player choses to fight, fight
@@ -68,9 +71,14 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+  }
+
+ 
 };
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
